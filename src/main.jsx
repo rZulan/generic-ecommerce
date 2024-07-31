4c9c31783e8778df901bafbb66cf44353502422e
RoiA2K <roi.zulan@gmail.com>
2024-07-31 10:57:35 +0800

    feat: tailwind; react-router-dom; MUI; theming

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ThemeProvider } from "@mui/material"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Checkout from "./Checkout.jsx"
+import HomePage from "./Home.jsx"
+import "./index.css"
+import Item from "./Item.jsx"
+import theme from "./theme.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+	{
+		path: "/item",
+		element: <Item />,
+	},
+	{
+		path: "/checkout",
+		element: <Checkout />,
+	},
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<RouterProvider router={router} />
+		</ThemeProvider>
+	</React.StrictMode>
 )
