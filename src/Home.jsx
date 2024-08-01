53bbf78bddae5f5cd2cbfe8af9539e53ff0cf71f
John Ford Actub <cs.actubjohnford@gmail.com>
2024-08-01 22:06:39 +0800

    feat: home page progress

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,24 @@
-import { Avatar, Link, Typography } from "@mui/material"
+import { Avatar, Button, Link, Typography } from "@mui/material"
+import itemUnavailable from './assets/item sample.jpg'
+import notFound from './assets/not found.jpg'
+import noData from './assets/no data.jpg'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const HomePage = () => {
-	return (
+
+
+const HomePage = () => {	
+		var settings = {
+			dots: true,
+			infinite: true,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 3500,
+			pauseOnHover: true
+		};
+		return (
 		<>
 		<div className="flex justify-center h-8 bg-sky-950">
 			<div className="pt-1">
@@ -10,7 +27,7 @@ const HomePage = () => {
 		</div>
 
 		<div className="flex flex-wrap">
-			<div className='flex pt-10 pl-10 gap-5 '>
+			<div className='flex flex-1 pt-10 pl-10 gap-5 '>
 				<div>
 					<Link href="/item" variant="h5">
 						<Typography variant="h5">Item</Typography>
@@ -27,16 +44,79 @@ const HomePage = () => {
 				</div>
 			</div>
 
-			<div className="flex justify-center pt-1">
+			<div className="flex flex-1 flex-grow justify-center pt-1">
 				<Typography variant="h2">Zulan</Typography>
 			</div>
 			
-			<div className="flex justify-end pt-8 pr-10">
+			<div className="flex flex-1 justify-end pt-8 pr-10">
 				<Avatar>A</Avatar>
 			</div>
 		</div>
 
+		<div className="flex justify-center pt-10"><Typography variant="h4">New Collection</Typography></div>
+		<div className="flex justify-center"><Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, facilis.</Typography></div>
 
+		<div className="grid grid-cols-3 gap-5 pt-5 w-3/4 m-auto text-center">
+			<div className="grid text-left ">
+				<img src={itemUnavailable} alt="item unavailable photo"/> 
+				<Typography variant="h6" ><b>Item Name</b></Typography>
+				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
+				<Typography variant="h6" display='block'><b>$0</b></Typography>
+			</div>
+
+			<div className="grid text-left ">
+				<img src={itemUnavailable} alt="item unavailable photo"/> 
+				<Typography variant="h6" ><b>Item Name</b></Typography>
+				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
+				<Typography variant="h6" display='block'><b>$0</b></Typography>
+			</div>
+
+			<div className="grid text-left ">
+				<img src={itemUnavailable} alt="item unavailable photo"/> 
+				<Typography variant="h6" ><b>Item Name</b></Typography>
+				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
+				<Typography variant="h6" display='block'><b>$0</b></Typography>
+			</div>
+
+			<div className="grid text-left ">
+				<img src={itemUnavailable} alt="item unavailable photo"/> 
+				<Typography variant="h6" ><b>Item Name</b></Typography>
+				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
+				<Typography variant="h6" display='block'><b>$0</b></Typography>
+			</div>
+
+			<div className="grid text-left ">
+				<img src={itemUnavailable} alt="item unavailable photo"/> 
+				<Typography variant="h6" ><b>Item Name</b></Typography>
+				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
+				<Typography variant="h6" display='block'><b>$0</b></Typography>
+			</div>
+
+			<div className="grid text-left ">
+				<img src={itemUnavailable} alt="item unavailable photo"/> 
+				<Typography variant="h6" ><b>Item Name</b></Typography>
+				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
+				<Typography variant="h6" display='block'><b>$0</b></Typography>
+			</div>
+		</div>
+
+		<div className="flex justify-center pt-10"><Typography variant="h4">Other Collection</Typography></div>
+		<div className="flex justify-center "><Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, facilis.</Typography></div>
+		<div className="flex justify-center pt-5"><Button variant="contained" sx={{borderRadius: 20, color: "white", backgroundColor: 'black'}}>Browse other collections</Button></div>
+		
+		
+		<div className="w-3/4 m-auto">
+			<div className="slider-container size-3/5 w-auto pt-10 gap-5">
+				<Slider {...settings}>
+					{/* <div className="grid grid-cols-3 gap-5 w-3/4 m-auto text-center"> */}
+						<div><img src={itemUnavailable} className="rounded-lg" alt="item unavailable photo"/></div>
+						<div><img src={noData} className="rounded-lg" alt="item unavailable photo"/></div>
+						<div><img src={notFound} className="rounded-lg" alt="item unavailable photo"/></div>
+					{/* </div> */}
+				</Slider>
+			</div>
+		</div>
+		
 		</>
 	)
 }
