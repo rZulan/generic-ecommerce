0278523c868132252312e943c2b076bb6c07c877
RoiA2K <roi.zulan@gmail.com>
2024-08-02 22:51:22 +0800

    feat: Layout Design - Item Page

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,4 +1,21 @@
+import { Box, Button, LinearProgress, Rating, Typography } from "@mui/material"
+import { useState } from "react"
 import Slider from "react-slick"
+
+function LinearProgressWithLabel(props) {
+	return (
+		<Box sx={{ display: "flex", alignItems: "center" }}>
+			<Box sx={{ width: 200, mr: 1 }}>
+				<LinearProgress variant="determinate" {...props} />
+			</Box>
+			<Box sx={{ minWidth: 35 }}>
+				<Typography variant="body2" color="text.secondary">{`${Math.round(
+					props.value
+				)} ratings`}</Typography>
+			</Box>
+		</Box>
+	)
+}
 
 const Item = () => {
 	const settings = {
@@ -9,8 +26,10 @@ const Item = () => {
 		slidesToScroll: 1,
 	}
 
+	const [rating, setRating] = useState(0)
+
 	return (
-		<>
+		<div className="w-3/4 m-auto">
 			<div className="flex flex-row h-16 text-white bg-black">
 				<ul className="flex flex-row flex-1 gap-5">
 					<li>Home</li>
@@ -22,8 +41,8 @@ const Item = () => {
 			<div className="flex">
 				<div className="flex flex-col bg-slate-400">
 					<img src="https://picsum.photos/400" className="w-96" />
-					<div className="m-auto mt-5 w-72">
-						<Slider {...settings}>
+					<div className="pb-10 m-auto mt-5 w-72">
+						<Slider {...settings} className="w-3/4 m-auto">
 							<div>
 								<img src="https://picsum.photos/1001" />
 							</div>
@@ -47,9 +66,87 @@ const Item = () => {
 				</div>
 				<div>
 					<h1>Item #1</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+						labore quas nemo dicta officia accusantium quisquam quidem mollitia
+						quibusdam consequuntur?
+					</p>
+					<div className="flex flex-row">
+						<Rating
+							value={rating}
+							onChange={(event, newValue) => {
+								setRating(newValue)
+							}}
+						/>
+						<p>500 ratings</p>
+					</div>
+					<h1>$69.00</h1>
+					<Button variant="contained">
+						<p>Buy Now</p>
+					</Button>
+					<Button variant="contained">
+						<p>Add to Cart</p>
+					</Button>
 				</div>
 			</div>
-		</>
+			<div className="mt-12">
+				<p>Product Details Lorem ipsum dolor sit amet.</p>
+				<ul>
+					<li>
+						• Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id,
+						molestias.
+					</li>
+					<li>• Lorem ipsum, dolor sit amet consectetur adipisicing.</li>
+					<li>
+						• Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, sint
+						voluptate.
+					</li>
+				</ul>
+				<img src="https://picsum.photos/600/200" />
+			</div>
+			<div className="mt-12">
+				<h1>Ratings and review Lorem ipsum dolor sit amet consectetur.</h1>
+				<div className="flex flex-row">
+					<div>
+						<h1>4.9/5</h1>
+						<Rating value={rating} />
+					</div>
+					<div>
+						<div className="flex flex-row">
+							<Rating value={5} />
+							<LinearProgressWithLabel value={100} />
+						</div>
+						<div className="flex flex-row">
+							<Rating value={1} />
+							<LinearProgressWithLabel value={100} />
+						</div>
+						<div className="flex flex-row">
+							<Rating value={1} />
+							<LinearProgressWithLabel value={100} />
+						</div>
+						<div className="flex flex-row">
+							<Rating value={2} />
+							<LinearProgressWithLabel value={100} />
+						</div>
+						<div className="flex flex-row">
+							<Rating value={1} />
+							<LinearProgressWithLabel value={100} />
+						</div>
+					</div>
+				</div>
+				<div className="mt-12 mb-96">
+					<Rating value={5} />
+					<h1>Lorem, ipsum.</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos ex
+						aspernatur aut id voluptatibus aliquid, eos laudantium excepturi
+						laborum rem velit, non maiores, nulla ipsam! Odio corrupti expedita
+						exercitationem molestiae?
+					</p>
+					<img src="https://picsum.photos/150/200" />
+				</div>
+			</div>
+		</div>
 	)
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { ThemeProvider } from "@mui/material"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Cart from "./cart.jsx"
 import Checkout from "./Checkout.jsx"
 import HomePage from "./Home.jsx"
 import "./index.css"
 import Item from "./Item.jsx"
 import theme from "./theme.jsx"
-import Cart from "./cart.jsx"
 
 const router = createBrowserRouter([
 	{
