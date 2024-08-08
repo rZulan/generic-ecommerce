89413cd6a709965d067096d0e95df27d78e39114
RoiA2K <roi.zulan@gmail.com>
2024-08-08 13:55:56 +0800

    feat: Browse Design

--- src/Browse.jsx
+++ src/Browse.jsx
@@ -1,30 +1,107 @@
+import {
+	Card,
+	CardActionArea,
+	CardContent,
+	CardMedia,
+	Rating,
+	Typography,
+} from "@mui/material"
 import Header from "./components/Header"
 
 const Browse = () => {
 	return (
 		<>
 			<Header />
-			<div className="w-3/4 m-auto">
-				<div>
-					<h1>Flash Sale!</h1>
-					<div className="flex flex-row gap-5">
-						<div className="w-48 h-48 bg-blue-400" />
-						<div className="w-48 h-48 bg-blue-400" />
-						<div className="w-48 h-48 bg-blue-400" />
-						<div className="w-48 h-48 bg-blue-400" />
-						<div className="w-48 h-48 bg-blue-400" />
-						<div className="w-48 h-48 bg-blue-400" />
+			<div className="w-2/3 m-auto">
+				<div className="mt-12">
+					<Typography variant="h4">Flash Sale!</Typography>
+					<div className="grid grid-cols-5 justify-items-center">
+						<Card sx={{ maxWidth: 300 }}>
+							<CardActionArea>
+								<CardMedia
+									component={"img"}
+									image="https://picsum.photos/300"
+								/>
+								<CardContent>
+									<Typography variant="h6">Item 1</Typography>
+									<Typography variant="body2">
+										Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
+										vero!
+									</Typography>
+								</CardContent>
+							</CardActionArea>
+						</Card>
+						<Card sx={{ maxWidth: 300 }}>
+							<CardActionArea>
+								<CardMedia
+									component={"img"}
+									height={"140"}
+									image="https://picsum.photos/3000"
+								/>
+								<CardContent>
+									<Typography variant="h6">Item 2</Typography>
+									<Typography variant="body2">
+										Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
+										vero! Lorem ipsum dolor sit amet.
+									</Typography>
+								</CardContent>
+							</CardActionArea>
+							<CardContent>
+								<Rating value={5} readOnly={true} />
+							</CardContent>
+						</Card>
+						<Card sx={{ maxWidth: 300 }}>
+							<CardActionArea>
+								<CardMedia
+									component={"img"}
+									height={"140"}
+									image="https://picsum.photos/3000"
+								/>
+								<CardContent>
+									<Typography variant="h6">Item 3</Typography>
+									<Typography variant="body2">
+										Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
+										vero! Lorem ipsum, dolor sit amet consectetur adipisicing.
+									</Typography>
+								</CardContent>
+							</CardActionArea>
+						</Card>
+						<Card sx={{ maxWidth: 300 }}>
+							<CardActionArea>
+								<CardMedia
+									component={"img"}
+									height={"140"}
+									image="https://picsum.photos/3000"
+								/>
+								<CardContent>
+									<Typography variant="h6">Item 4</Typography>
+									<Typography variant="body2">
+										Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									</Typography>
+								</CardContent>
+							</CardActionArea>
+						</Card>
+						<Card sx={{ maxWidth: 300 }}>
+							<CardActionArea>
+								<CardMedia
+									component={"img"}
+									height={"140"}
+									image="https://picsum.photos/3000"
+								/>
+								<CardContent>
+									<Typography variant="h6">Item 5</Typography>
+									<Typography variant="body2">
+										Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
+										vero!
+									</Typography>
+								</CardContent>
+							</CardActionArea>
+						</Card>
 					</div>
 				</div>
 				<div>
 					<h1>Categories</h1>
 					<div className="grid grid-cols-6 gap-2">
-						<div className="w-48 h-48 bg-blue-400" />
-						<div className="w-48 h-48 bg-blue-400" />
-						<div className="w-48 h-48 bg-blue-400" />
-						<div className="w-48 h-48 bg-blue-400" />
-						<div className="w-48 h-48 bg-blue-400" />
-						<div className="w-48 h-48 bg-blue-400" />
 						<div className="w-48 h-48 bg-blue-400" />
 						<div className="w-48 h-48 bg-blue-400" />
 						<div className="w-48 h-48 bg-blue-400" />
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,297 +1,394 @@
-import { Avatar, Box, Button, Link, Rating, Typography } from "@mui/material"
-import itemUnavailable from './assets/item sample.jpg'
-import notFound from './assets/not found.jpg'
-import noData from './assets/no data.jpg'
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { useState } from "react";
-import StarIcon from '@mui/icons-material/Star';
-import Header from "./components/Header";
+import StarIcon from "@mui/icons-material/Star"
+import { Box, Button, Link, Rating, Typography } from "@mui/material"
+import { useState } from "react"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css"
+import itemUnavailable from "./assets/item sample.jpg"
+import noData from "./assets/no data.jpg"
+import notFound from "./assets/not found.jpg"
+import Header from "./components/Header"
 
 const labels = {
-	0.5: 'Useless',
-	1: 'Useless+',
-	1.5: 'Poor',
-	2: 'Poor+',
-	2.5: 'Ok',
-	3: 'Ok+',
-	3.5: 'Good',
-	4: 'Good+',
-	4.5: 'Excellent',
-	5: 'Excellent+',
-};
-
-function getLabelText(value) {
-	return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+	0.5: "Useless",
+	1: "Useless+",
+	1.5: "Poor",
+	2: "Poor+",
+	2.5: "Ok",
+	3: "Ok+",
+	3.5: "Good",
+	4: "Good+",
+	4.5: "Excellent",
+	5: "Excellent+",
 }
 
-const HomePage = () => {	
-		var settings = {
-			dots: true,
-			infinite: true,
-			slidesToShow: 3,
-			slidesToScroll: 1,
-			autoplay: true,
-			autoplaySpeed: 3500,
-			pauseOnHover: true
-		};
+function getLabelText(value) {
+	return `${value} Star${value !== 1 ? "s" : ""}, ${labels[value]}`
+}
 
-		const [value1, setValue1] = useState(4);
-		const [hover1, setHover1] = useState(-1);
+const HomePage = () => {
+	var settings = {
+		dots: true,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3500,
+		pauseOnHover: true,
+	}
 
-		const [value2, setValue2] = useState(4);
-		const [hover2, setHover2] = useState(-1);
+	const [value1, setValue1] = useState(4)
+	const [hover1, setHover1] = useState(-1)
 
-		const [value3, setValue3] = useState(4);
-		const [hover3, setHover3] = useState(-1);
+	const [value2, setValue2] = useState(4)
+	const [hover2, setHover2] = useState(-1)
 
-		const [value4, setValue4] = useState(4);
-		const [hover4, setHover4] = useState(-1);
+	const [value3, setValue3] = useState(4)
+	const [hover3, setHover3] = useState(-1)
 
-		const [value5, setValue5] = useState(4);
-		const [hover5, setHover5] = useState(-1);
+	const [value4, setValue4] = useState(4)
+	const [hover4, setHover4] = useState(-1)
 
-		const [value6, setValue6] = useState(4);
-		const [hover6, setHover6] = useState(-1);
+	const [value5, setValue5] = useState(4)
+	const [hover5, setHover5] = useState(-1)
 
-		return (
+	const [value6, setValue6] = useState(4)
+	const [hover6, setHover6] = useState(-1)
+
+	return (
 		<>
-		<div className="flex justify-center h-8 bg-sky-950">
-			<div className="pt-1">
-			<Typography color={'white'}>Get up to 70% off on selected items!</Typography>
-			</div>
-		</div>
-
-		<Header/>
-
-		<div className="flex justify-center pt-10"><Typography variant="h4">New Collection</Typography></div>
-		<div className="flex justify-center"><Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, facilis.</Typography></div>
-
-		<div className="grid grid-cols-3 gap-5 pt-5 w-3/4 m-auto text-center">
-			<div className="grid text-left ">
-				<img src={itemUnavailable} alt="item unavailable photo"/> 
-				<Typography variant="h6" ><b>Item Name</b></Typography>
-				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
-				<Typography variant="h6" display='block'><b>$0</b></Typography>
-				<Box
-					sx={{
-						width: 200,
-						display: 'flex',
-						alignItems: 'center',
-					}}
-					>
-					<Rating
-						name="hover-feedback"
-						value={value1}
-						precision={0.5}
-						getLabelText={getLabelText}
-						onChange={(event, newValue) => {
-						setValue1(newValue);
-						}}
-						onChangeActive={(event, newHover) => {
-						setHover1(newHover);
-						}}
-						emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
-					/>
-					{value1 !== null && (
-						<Box sx={{ ml: 2 }}>{labels[hover1 !== -1 ? hover1 : value1]}</Box>
-					)}
-				</Box>
-			</div>
-
-			<div className="grid text-left ">
-				<img src={itemUnavailable} alt="item unavailable photo"/> 
-				<Typography variant="h6" ><b>Item Name</b></Typography>
-				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
-				<Typography variant="h6" display='block'><b>$0</b></Typography>
-				<Box
-					sx={{
-						width: 200,
-						display: 'flex',
-						alignItems: 'center',
-					}}
-					>
-					<Rating
-						name="hover-feedback"
-						value={value2}
-						precision={0.5}
-						getLabelText={getLabelText}
-						onChange={(event, newValue) => {
-						setValue2(newValue);
-						}}
-						onChangeActive={(event, newHover) => {
-						setHover2(newHover);
-						}}
-						emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
-					/>
-					{value2 !== null && (
-						<Box sx={{ ml: 2 }}>{labels[hover2 !== -1 ? hover2 : value2]}</Box>
-					)}
-				</Box>
-			</div>
-
-			<div className="grid text-left ">
-				<img src={itemUnavailable} alt="item unavailable photo"/> 
-				<Typography variant="h6" ><b>Item Name</b></Typography>
-				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
-				<Typography variant="h6" display='block'><b>$0</b></Typography>
-				<Box
-					sx={{
-						width: 200,
-						display: 'flex',
-						alignItems: 'center',
-					}}
-					>
-					<Rating
-						name="hover-feedback"
-						value={value3}
-						precision={0.5}
-						getLabelText={getLabelText}
-						onChange={(event, newValue) => {
-						setValue3(newValue);
-						}}
-						onChangeActive={(event, newHover) => {
-						setHover3(newHover);
-						}}
-						emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
-					/>
-					{value3 !== null && (
-						<Box sx={{ ml: 2 }}>{labels[hover3 !== -1 ? hover3 : value3]}</Box>
-					)}
-				</Box>
-			</div>
-
-			<div className="grid text-left ">
-				<img src={itemUnavailable} alt="item unavailable photo"/> 
-				<Typography variant="h6" ><b>Item Name</b></Typography>
-				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
-				<Typography variant="h6" display='block'><b>$0</b></Typography>
-				<Box
-					sx={{
-						width: 200,
-						display: 'flex',
-						alignItems: 'center',
-					}}
-					>
-					<Rating
-						name="hover-feedback"
-						value={value4}
-						precision={0.5}
-						getLabelText={getLabelText}
-						onChange={(event, newValue) => {
-						setValue4(newValue);
-						}}
-						onChangeActive={(event, newHover) => {
-						setHover4(newHover);
-						}}
-						emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
-					/>
-					{value4 !== null && (
-						<Box sx={{ ml: 2 }}>{labels[hover4 !== -1 ? hover4 : value4]}</Box>
-					)}
-				</Box>
-			</div>
-
-			<div className="grid text-left ">
-				<img src={itemUnavailable} alt="item unavailable photo"/> 
-				<Typography variant="h6" ><b>Item Name</b></Typography>
-				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
-				<Typography variant="h6" display='block'><b>$0</b></Typography>
-				<Box
-					sx={{
-						width: 200,
-						display: 'flex',
-						alignItems: 'center',
-					}}
-					>
-					<Rating
-						name="hover-feedback"
-						value={value5}
-						precision={0.5}
-						getLabelText={getLabelText}
-						onChange={(event, newValue) => {
-						setValue5(newValue);
-						}}
-						onChangeActive={(event, newHover) => {
-						setHover5(newHover);
-						}}
-						emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
-					/>
-					{value5 !== null && (
-						<Box sx={{ ml: 2 }}>{labels[hover5 !== -1 ? hover5 : value5]}</Box>
-					)}
-				</Box>
-			</div>
-
-			<div className="grid text-left ">
-				<img src={itemUnavailable} alt="item unavailable photo"/> 
-				<Typography variant="h6" ><b>Item Name</b></Typography>
-				<Typography variant="caption" display='block'>This is the description of the item.</Typography>
-				<Typography variant="h6" display='block'><b>$0</b></Typography>
-				<Box
-					sx={{
-						width: 200,
-						display: 'flex',
-						alignItems: 'center',
-					}}
-					>
-					<Rating
-						name="hover-feedback"
-						value={value6}
-						precision={0.5}
-						getLabelText={getLabelText}
-						onChange={(event, newValue) => {
-						setValue6(newValue);
-						}}
-						onChangeActive={(event, newHover) => {
-						setHover6(newHover);
-						}}
-						emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
-					/>
-					{value6 !== null && (
-						<Box sx={{ ml: 2 }}>{labels[hover6 !== -1 ? hover6 : value6]}</Box>
-					)}
-				</Box>
-			</div>
-		</div>
-
-		<div className="flex justify-center pt-10"><Typography variant="h4">Other Collection</Typography></div>
-		<div className="flex justify-center "><Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, facilis.</Typography></div>
-		<div className="flex justify-center pt-5"><Button variant="contained" sx={{borderRadius: 20, color: "white", backgroundColor: 'black'}}>Browse other collections</Button></div>
-		
-		
-		<div className="w-3/4 m-auto">
-			<div className="slider-container size-3/5 w-auto pt-10 gap-5">
-				<Slider {...settings}>
-					{/* <div className="grid grid-cols-3 gap-5 w-3/4 m-auto text-center"> */}
-						<div><img src={itemUnavailable} className="rounded-lg" alt="item unavailable photo"/></div>
-						<div><img src={noData} className="rounded-lg" alt="item unavailable photo"/></div>
-						<div><img src={notFound} className="rounded-lg" alt="item unavailable photo"/></div>
-					{/* </div> */}
-				</Slider>
-			</div>
-		</div>
-		
-		<div className="flex flex-wrap h-16 bg-sky-950 mt-10">
-			<div className="pl-10 pt-5">
-				<Typography color={'white'}>© 2024 Zulan Productions</Typography>
-			</div>
-
-			<div className="flex flex-1 justify-end gap-9 pr-10">
-				<div className="pl-5 pt-5">
-					<Link>
-						<Typography color={'white'}>Terms and Conditions</Typography>
-					</Link>
-				</div>
-				<div className="pl-5 pt-5">
-					<Link>
-						<Typography color={'white'}>Privacy Policy</Typography>
-					</Link>
+			<div className="flex justify-center h-8 bg-sky-950">
+				<div className="pt-1">
+					<Typography color={"white"}>
+						Get up to 70% off on selected items!
+					</Typography>
 				</div>
 			</div>
 
-		</div>
+			<Header />
+
+			<div className="flex justify-center pt-10">
+				<Typography variant="h4">New Collection</Typography>
+			</div>
+			<div className="flex justify-center">
+				<Typography>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, facilis.
+				</Typography>
+			</div>
+
+			<div className="grid w-3/4 grid-cols-3 gap-5 pt-5 m-auto text-center">
+				<div className="grid text-left ">
+					<img src={itemUnavailable} alt="item unavailable photo" />
+					<Typography variant="h6">
+						<b>Item Name</b>
+					</Typography>
+					<Typography variant="caption" display="block">
+						This is the description of the item.
+					</Typography>
+					<Typography variant="h6" display="block">
+						<b>$0</b>
+					</Typography>
+					<Box
+						sx={{
+							width: 200,
+							display: "flex",
+							alignItems: "center",
+						}}
+					>
+						<Rating
+							name="hover-feedback"
+							value={value1}
+							precision={0.5}
+							getLabelText={getLabelText}
+							onChange={(event, newValue) => {
+								setValue1(newValue)
+							}}
+							onChangeActive={(event, newHover) => {
+								setHover1(newHover)
+							}}
+							emptyIcon={
+								<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+							}
+						/>
+						{value1 !== null && (
+							<Box sx={{ ml: 2 }}>
+								{labels[hover1 !== -1 ? hover1 : value1]}
+							</Box>
+						)}
+					</Box>
+				</div>
+
+				<div className="grid text-left ">
+					<img src={itemUnavailable} alt="item unavailable photo" />
+					<Typography variant="h6">
+						<b>Item Name</b>
+					</Typography>
+					<Typography variant="caption" display="block">
+						This is the description of the item.
+					</Typography>
+					<Typography variant="h6" display="block">
+						<b>$0</b>
+					</Typography>
+					<Box
+						sx={{
+							width: 200,
+							display: "flex",
+							alignItems: "center",
+						}}
+					>
+						<Rating
+							name="hover-feedback"
+							value={value2}
+							precision={0.5}
+							getLabelText={getLabelText}
+							onChange={(event, newValue) => {
+								setValue2(newValue)
+							}}
+							onChangeActive={(event, newHover) => {
+								setHover2(newHover)
+							}}
+							emptyIcon={
+								<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+							}
+						/>
+						{value2 !== null && (
+							<Box sx={{ ml: 2 }}>
+								{labels[hover2 !== -1 ? hover2 : value2]}
+							</Box>
+						)}
+					</Box>
+				</div>
+
+				<div className="grid text-left ">
+					<img src={itemUnavailable} alt="item unavailable photo" />
+					<Typography variant="h6">
+						<b>Item Name</b>
+					</Typography>
+					<Typography variant="caption" display="block">
+						This is the description of the item.
+					</Typography>
+					<Typography variant="h6" display="block">
+						<b>$0</b>
+					</Typography>
+					<Box
+						sx={{
+							width: 200,
+							display: "flex",
+							alignItems: "center",
+						}}
+					>
+						<Rating
+							name="hover-feedback"
+							value={value3}
+							precision={0.5}
+							getLabelText={getLabelText}
+							onChange={(event, newValue) => {
+								setValue3(newValue)
+							}}
+							onChangeActive={(event, newHover) => {
+								setHover3(newHover)
+							}}
+							emptyIcon={
+								<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+							}
+						/>
+						{value3 !== null && (
+							<Box sx={{ ml: 2 }}>
+								{labels[hover3 !== -1 ? hover3 : value3]}
+							</Box>
+						)}
+					</Box>
+				</div>
+
+				<div className="grid text-left ">
+					<img src={itemUnavailable} alt="item unavailable photo" />
+					<Typography variant="h6">
+						<b>Item Name</b>
+					</Typography>
+					<Typography variant="caption" display="block">
+						This is the description of the item.
+					</Typography>
+					<Typography variant="h6" display="block">
+						<b>$0</b>
+					</Typography>
+					<Box
+						sx={{
+							width: 200,
+							display: "flex",
+							alignItems: "center",
+						}}
+					>
+						<Rating
+							name="hover-feedback"
+							value={value4}
+							precision={0.5}
+							getLabelText={getLabelText}
+							onChange={(event, newValue) => {
+								setValue4(newValue)
+							}}
+							onChangeActive={(event, newHover) => {
+								setHover4(newHover)
+							}}
+							emptyIcon={
+								<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+							}
+						/>
+						{value4 !== null && (
+							<Box sx={{ ml: 2 }}>
+								{labels[hover4 !== -1 ? hover4 : value4]}
+							</Box>
+						)}
+					</Box>
+				</div>
+
+				<div className="grid text-left ">
+					<img src={itemUnavailable} alt="item unavailable photo" />
+					<Typography variant="h6">
+						<b>Item Name</b>
+					</Typography>
+					<Typography variant="caption" display="block">
+						This is the description of the item.
+					</Typography>
+					<Typography variant="h6" display="block">
+						<b>$0</b>
+					</Typography>
+					<Box
+						sx={{
+							width: 200,
+							display: "flex",
+							alignItems: "center",
+						}}
+					>
+						<Rating
+							name="hover-feedback"
+							value={value5}
+							precision={0.5}
+							getLabelText={getLabelText}
+							onChange={(event, newValue) => {
+								setValue5(newValue)
+							}}
+							onChangeActive={(event, newHover) => {
+								setHover5(newHover)
+							}}
+							emptyIcon={
+								<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+							}
+						/>
+						{value5 !== null && (
+							<Box sx={{ ml: 2 }}>
+								{labels[hover5 !== -1 ? hover5 : value5]}
+							</Box>
+						)}
+					</Box>
+				</div>
+
+				<div className="grid text-left ">
+					<img src={itemUnavailable} alt="item unavailable photo" />
+					<Typography variant="h6">
+						<b>Item Name</b>
+					</Typography>
+					<Typography variant="caption" display="block">
+						This is the description of the item.
+					</Typography>
+					<Typography variant="h6" display="block">
+						<b>$0</b>
+					</Typography>
+					<Box
+						sx={{
+							width: 200,
+							display: "flex",
+							alignItems: "center",
+						}}
+					>
+						<Rating
+							name="hover-feedback"
+							value={value6}
+							precision={0.5}
+							getLabelText={getLabelText}
+							onChange={(event, newValue) => {
+								setValue6(newValue)
+							}}
+							onChangeActive={(event, newHover) => {
+								setHover6(newHover)
+							}}
+							emptyIcon={
+								<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+							}
+						/>
+						{value6 !== null && (
+							<Box sx={{ ml: 2 }}>
+								{labels[hover6 !== -1 ? hover6 : value6]}
+							</Box>
+						)}
+					</Box>
+				</div>
+			</div>
+
+			<div className="flex justify-center pt-10">
+				<Typography variant="h4">Other Collection</Typography>
+			</div>
+			<div className="flex justify-center ">
+				<Typography>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, facilis.
+				</Typography>
+			</div>
+			<div className="flex justify-center pt-5">
+				<Button
+					variant="contained"
+					sx={{ borderRadius: 20, color: "white", backgroundColor: "black" }}
+				>
+					Browse other collections
+				</Button>
+			</div>
+
+			<div className="w-3/4 m-auto">
+				<div className="w-auto gap-5 pt-10 slider-container size-3/5">
+					<Slider {...settings}>
+						{/* <div className="grid w-3/4 grid-cols-3 gap-5 m-auto text-center"> */}
+						<div>
+							<img
+								src={itemUnavailable}
+								className="rounded-lg"
+								alt="item unavailable photo"
+							/>
+						</div>
+						<div>
+							<img
+								src={noData}
+								className="rounded-lg"
+								alt="item unavailable photo"
+							/>
+						</div>
+						<div>
+							<img
+								src={notFound}
+								className="rounded-lg"
+								alt="item unavailable photo"
+							/>
+						</div>
+						{/* </div> */}
+					</Slider>
+				</div>
+			</div>
+
+			<div className="flex flex-wrap h-16 mt-10 bg-sky-950">
+				<div className="pt-5 pl-10">
+					<Typography color={"white"}>© 2024 Zulan Productions</Typography>
+				</div>
+
+				<div className="flex justify-end flex-1 pr-10 gap-9">
+					<div className="pt-5 pl-5">
+						<Link>
+							<Typography color={"white"}>Terms and Conditions</Typography>
+						</Link>
+					</div>
+					<div className="pt-5 pl-5">
+						<Link>
+							<Typography color={"white"}>Privacy Policy</Typography>
+						</Link>
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
